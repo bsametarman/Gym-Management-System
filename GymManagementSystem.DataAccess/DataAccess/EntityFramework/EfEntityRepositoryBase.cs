5d8edd142fb53a4433c9f39676d3d746411aae75
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-14 12:12:45 +0300

    dataaccess layer added

---
+++ GymManagementSystem.DataAccess/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,69 @@
+﻿using GymManagementSystem.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.DataAccess.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State= EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public async Task<TEntity> Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                //return await context.Set<TEntity>().Where(filter).SingleOrDefault();
+                if (filter == null)
+                    return await context.Set<TEntity>().SingleOrDefaultAsync();
+                else
+                    return await context.Set<TEntity>().Where(filter).SingleOrDefaultAsync();
+            }
+        }
+
+        public async Task<List<TEntity>> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                if (filter == null)
+                    return await context.Set<TEntity>().ToListAsync();
+                else
+                    return await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ GymManagementSystem.DataAccess/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementSystem.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.DataAccess.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<List<T>> GetAll(Expression<Func<T, bool>> filter = null);
+        Task<T> Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
