20c407991fa588d335625ab85e4ae682c6bb6ce5
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-14 15:46:16 +0300

    mvc web ui added, packages added

---
+++ GymManagementSystem.MVCWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymManagementSystem.MVCWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
