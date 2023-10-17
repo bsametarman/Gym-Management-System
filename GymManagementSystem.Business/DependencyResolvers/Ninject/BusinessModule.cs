09017344da709c1ad23a62a5ec101309a83cbccf
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-17 22:46:44 +0300

    dependency resolver added

---
+++ GymManagementSystem.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Business.Concrete;
+using GymManagementSystem.DataAccess.Abstract;
+using GymManagementSystem.DataAccess.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+
+            Bind<DbContext>().To<GymManagementSystemDbContext>().InSingletonScope();
+
+            Bind<IUserService>().To<UserManager>().InSingletonScope();
+            Bind<IUserDal>().To<EfUserDal>().InSingletonScope();
+
+        }
+    }
+}
