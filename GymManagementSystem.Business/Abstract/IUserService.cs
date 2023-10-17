e594f5deb477cb8ed631c01f73d67406c26aa5d4
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-17 23:00:32 +0300

    user methods added

---
+++ GymManagementSystem.Business/Abstract/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<AppUser>> GetAll();
+        IDataResult<AppUser> GetById(string id);
+        IResult GetByEmailAndPassword(string email, string password);
+        IResult Add(AppUser user);
+        IResult Update(AppUser user);
+        IResult Delete(AppUser user);
+    }
+}
---
+++ GymManagementSystem.Business/Concrete/UserManager.cs
@@ -0,0 +1,61 @@
+﻿using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.DataAccess.Abstract;
+using GymManagementSystem.DataAccess.Concrete;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(AppUser user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult("Başarıyla eklendi !!!");
+        }
+
+        public IResult Delete(AppUser user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult("Başarıyla silindi !!!");
+        }
+
+        public IDataResult<AppUser> GetById(string id)
+        {
+            return new SuccessDataResult<AppUser>(_userDal.Get(u => u.Id == id), "Başarıyla listelendi !!!");
+        }
+
+        public IResult GetByEmailAndPassword(string email, string password)
+        {
+            var result = _userDal.GetAll(u => u.Email == email && u.Password == password);
+
+            if (result.Count() != 0)
+                return new SuccessResult("Başarıyla getirildi !!!");
+            else
+                return new ErrorResult("Kişi bulunumadı!!!");
+        }
+
+        public IDataResult<List<AppUser>> GetAll()
+        {
+            return new SuccessDataResult<List<AppUser>>(_userDal.GetAll(), "Başarıyla listelendi !!!");
+        }
+
+        public IResult Update(AppUser user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult("Başarıyla güncellendi !!!");
+        }
+    }
+}
