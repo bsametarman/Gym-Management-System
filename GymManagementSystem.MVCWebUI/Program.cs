4c68ae251f372bbc73fbb312194bf9bee2222fc0
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-18 17:06:55 +0300

    changes for user identification

--- GymManagementSystem.MVCWebUI/Program.cs
+++ GymManagementSystem.MVCWebUI/Program.cs
@@ -1,20 +1,82 @@
+using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Business.Concrete;
+using GymManagementSystem.DataAccess.Abstract;
+using GymManagementSystem.DataAccess.Concrete;
+using GymManagementSystem.Entities.Concrete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddTransient<IUserService, UserManager>();
+builder.Services.AddTransient<IUserDal, EfUserDal>();
+
+builder.Services.AddDbContext<GymManagementSystemDbContext>();
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<GymManagementSystemDbContext>();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Expiration = TimeSpan.FromMinutes(30);
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        //options.LoginPath = "/Account/Login";
+        //options.LogoutPath = "/Account/Logout";
+    });
+
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Default Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    await Seed.SeedAdminAndRoleAsync(app);
 }
 
-app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//	app.UseExceptionHandler("/Home/Error");
+//	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//	app.UseHsts();
+//}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseDeveloperExceptionPage();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
+//app.UseSession(); ***
+app.UseAuthentication();
 
 app.UseRouting();
 
