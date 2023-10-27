7711127be190f8554afb7234140a0ce65e1e55db
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-27 19:55:35 +0300

    business and data access layers added for enter log

---
+++ GymManagementSystem.Business/Abstract/IEnterLogService.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Abstract
+{
+    public interface IEnterLogService
+    {
+        IDataResult<List<EnterLog>> GetAll();
+        IDataResult<EnterLog> GetById(int id);
+        IResult Add(EnterLog enterLog);
+        IResult Update(EnterLog enterLog);
+        IResult Delete(EnterLog enterLog);
+    }
+}
---
+++ GymManagementSystem.Business/Concrete/EnterLogManager.cs
@@ -0,0 +1,56 @@
+﻿using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.DataAccess.Abstract;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Concrete
+{
+    public class EnterLogManager : IEnterLogService
+    {
+        private IEnterLogDal _enterLogDal;
+
+        public EnterLogManager(IEnterLogDal enterLogDal)
+        {
+            _enterLogDal = enterLogDal;
+        }
+
+        public IResult Add(EnterLog enterLog)
+        {
+            _enterLogDal.Add(enterLog);
+            return new SuccessResult("Başarıyla eklendi !");
+        }
+
+        public IResult Delete(EnterLog enterLog)
+        {
+            _enterLogDal.Delete(enterLog);
+            return new SuccessResult("Başarıyla silindi !");
+        }
+
+        public IDataResult<List<EnterLog>> GetAll()
+        {
+            var result = _enterLogDal.GetAll();
+            if (result.Count != 0)
+                return new SuccessDataResult<List<EnterLog>>(result, "Başarıyla listelendi !");
+            return new ErrorDataResult<List<EnterLog>>(result, "Bir hata oluştu !");
+        }
+
+        public IDataResult<EnterLog> GetById(int id)
+        {
+            var result = _enterLogDal.Get(log => log.Id == id);
+            if (result != null)
+                return new SuccessDataResult<EnterLog>(result, "Başarıyla listelendi !");
+            return new ErrorDataResult<EnterLog>(result, "Bir hata oluştu !");
+        }
+
+        public IResult Update(EnterLog enterLog)
+        {
+            _enterLogDal.Update(enterLog);
+            return new SuccessResult("Başarıyla güncellendi !");
+        }
+    }
+}
