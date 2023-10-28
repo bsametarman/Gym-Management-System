b4a3f6cd2fa7802ee67a6e4c0bad380beb9e5b48
bsametarman@outlook.com <bsametarman@outlook.com>
2023-10-28 15:19:25 +0300

    layers added for trainer

---
+++ GymManagementSystem.Business/Abstract/ITrainerService.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Abstract
+{
+    public interface ITrainerService
+    {
+        IDataResult<List<Trainer>> GetAll();
+        IDataResult<Trainer> GetById(int id);
+        IResult Add(Trainer trainer);
+        IResult Update(Trainer trainer);
+        IResult Delete(Trainer trainer);
+    }
+}
---
+++ GymManagementSystem.Business/Concrete/TrainerManager.cs
@@ -0,0 +1,57 @@
+﻿using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Core.Utilities.Results;
+using GymManagementSystem.DataAccess.Abstract;
+using GymManagementSystem.DataAccess.Concrete;
+using GymManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Business.Concrete
+{
+    public class TrainerManager : ITrainerService
+    {
+        private ITrainerDal _trainerDal;
+
+        public TrainerManager(ITrainerDal trainerDal)
+        {
+            _trainerDal = trainerDal;
+        }
+
+        public IResult Add(Trainer trainer)
+        {
+            _trainerDal.Add(trainer);
+            return new SuccessResult("Başarıyla eklendi !");
+        }
+
+        public IResult Delete(Trainer trainer)
+        {
+            _trainerDal.Delete(trainer);
+            return new SuccessResult("Başarıyla silindi !");
+        }
+
+        public IDataResult<List<Trainer>> GetAll()
+        {
+            var result = _trainerDal.GetAll();
+            if (result.Count != 0)
+                return new SuccessDataResult<List<Trainer>>(result, "Başarıyla listelendi !");
+            return new ErrorDataResult<List<Trainer>>(result, "Bir hata oluştu !");
+        }
+
+        public IDataResult<Trainer> GetById(int id)
+        {
+            var result = _trainerDal.Get(log => log.Id == id);
+            if (result != null)
+                return new SuccessDataResult<Trainer>(result, "Başarıyla listelendi !");
+            return new ErrorDataResult<Trainer>(result, "Bir hata oluştu !");
+        }
+
+        public IResult Update(Trainer trainer)
+        {
+            _trainerDal.Update(trainer);
+            return new SuccessResult("Başarıyla güncellendi !");
+        }
+    }
+}
