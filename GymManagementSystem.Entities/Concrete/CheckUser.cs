2bb9773dbb9c44590ee0ef8f3381dc604ee6c556
bsametarman@outlook.com <bsametarman@outlook.com>
2023-11-07 11:40:41 +0300

    new entity added for user check

---
+++ GymManagementSystem.Entities/Concrete/CheckUser.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementSystem.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Entities.Concrete
+{
+	public class CheckUser : IEntity
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string UserName { get; set; }
+		public int EnterCount { get; set; }
+		public DateTime LastPaymentDate { get; set; }
+		public DateTime MembershipExpirationDate { get; set; }
+	}
+}
