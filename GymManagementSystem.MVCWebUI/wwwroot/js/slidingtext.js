9915e357aa521c5d297e88031757c4e94949f588
bsametarman@outlook.com <bsametarman@outlook.com>
2023-12-09 16:14:38 +0300

    layout changes, added sliding text

---
+++ GymManagementSystem.MVCWebUI/wwwroot/js/slidingtext.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let slidingText = document.getElementById("text-sliding");
+
+    setInterval(function () {
+        changeColor(slidingText);
+    }, 1000);
+});
+
+function changeColor(element) {
+    let currentColor = window.getComputedStyle(element).color;
+    let colorToChange = (currentColor === "rgb(150, 14, 24)") ? "rgb(52, 47, 109)" : "rgb(150, 14, 24)";
+
+    element.style.color = colorToChange;
+}
