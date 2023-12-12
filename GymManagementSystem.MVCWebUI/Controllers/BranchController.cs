7dd31a88e2a88146e17dbb9ee77a8fe9c5fac985
bsametarman@outlook.com <bsametarman@outlook.com>
2023-12-12 21:05:22 +0300

    branch methods and pages added, some visual changes

---
+++ GymManagementSystem.MVCWebUI/Controllers/BranchController.cs
@@ -0,0 +1,27 @@
+﻿using GymManagementSystem.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementSystem.MVCWebUI.Controllers
+{
+    public class BranchController : Controller
+    {
+        private IBranchService _branchService;
+
+        public BranchController(IBranchService branchService)
+        {
+            _branchService = branchService;
+        }
+
+        public IActionResult Index()
+        {
+            var branches = _branchService.GetAll();
+            return View(branches.Data);
+        }
+
+        public IActionResult Detail(int id) 
+        {
+            var branch = _branchService.GetById(id);
+            return View(branch.Data);
+        }
+    }
+}
