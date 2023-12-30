dd82db8a75db27ab97d3edb3aa7fdb986445809e
bsametarman@outlook.com <bsametarman@outlook.com>
2023-12-30 17:05:03 +0300

    validation error class added

---
+++ GymManagementSystem.Core/Utilities/Errors/ValidationError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Core.Utilities.Errors
+{
+    public class ValidationError
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
