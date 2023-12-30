9ebd0b8a8d764eb34870d4d6ee4e1564c5c12e61
bsametarman@outlook.com <bsametarman@outlook.com>
2023-12-30 11:07:38 +0300

    changes for user sign up

--- GymManagementSystem.MVCWebUI/Models/UserRegisterViewModel.cs
+++ GymManagementSystem.MVCWebUI/Models/UserRegisterViewModel.cs
@@ -3,16 +3,16 @@
     public class UserRegisterViewModel
     {
         public string Name { get; set; }
-        public string Surname { get;set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+		public string Surname { get;set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
         public string Gender { get; set; }
         public string YearOfBirth { get; set; }
-        public string IdentityNumber { get; set; }
-        public string Address { get; set; }
-        //public string BloodType { get; set; }
+		public string IdentityNumber { get; set; }
+		public string Address { get; set; }
+        public int BloodTypeId { get; set; }
         public string EmergencyPhoneNumber { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+		public string PhoneNumber { get; set; }
     }
 }
