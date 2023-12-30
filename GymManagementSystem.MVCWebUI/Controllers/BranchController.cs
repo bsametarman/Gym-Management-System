3bc2d46bf3b57dd0017f4afdb8148cd222979c03
bsametarman@outlook.com <bsametarman@outlook.com>
2023-12-30 17:06:13 +0300

    branch add page added, changes for branch

--- GymManagementSystem.MVCWebUI/Controllers/BranchController.cs
+++ GymManagementSystem.MVCWebUI/Controllers/BranchController.cs
@@ -1,4 +1,8 @@
 ﻿using GymManagementSystem.Business.Abstract;
+using GymManagementSystem.Core.Utilities.Errors;
+using GymManagementSystem.Entities.Concrete;
+using GymManagementSystem.MVCWebUI.Tools.Validation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementSystem.MVCWebUI.Controllers
@@ -22,6 +26,66 @@ namespace GymManagementSystem.MVCWebUI.Controllers
         {
             var branch = _branchService.GetById(id);
             return View(branch.Data);
+        }
+
+        public IActionResult AddBranch()
+        {
+            if (User.IsInRole("member"))
+                return RedirectToAction("Account", "Dashboard");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBranch(Branch branch)
+        {
+            List<string> errors = new List<string>();
+            List<List<ValidationError>> validationErrors = new List<List<ValidationError>>();
+
+            if(!ModelState.IsValid)
+                return View(branch);
+
+            branch.IsActive = true;
+            branch.NumberOfMembers = 0;
+            branch.Price = 0;
+
+            BranchValidator branchValidator = new BranchValidator(_branchService);
+
+			validationErrors.Add(branchValidator.CheckName(branch.Name));
+			validationErrors.Add(branchValidator.CheckDescription(branch.Description));
+			validationErrors.Add(branchValidator.CheckEmail(branch.Email));
+			validationErrors.Add(branchValidator.CheckPhoneNumber(branch.PhoneNumber));
+			validationErrors.Add(branchValidator.CheckCity(branch.City));
+			validationErrors.Add(branchValidator.CheckAddress(branch.Address));
+			validationErrors.Add(branchValidator.CheckCapacity(branch.Capacity));
+			validationErrors.Add(branchValidator.CheckCountry(branch.Country));
+			validationErrors.Add(branchValidator.CheckNumberOfEmployee(branch.NumberOfEmployee));
+			validationErrors.Add(branchValidator.CheckNumberOfTools(branch.NumberOfTools));
+			validationErrors.Add(branchValidator.CheckSquareMeters(branch.SquareMeters));
+			validationErrors.Add(branchValidator.CheckBranchWorkingTime(branch.WeekdaysOpeningTime, branch.WeekdaysClosingTime, branch.WeekendsOpeningTime, branch.WeekendsClosingTime));
+
+            foreach (var results in validationErrors)
+            {
+                if (results != null)
+					foreach (var error in results)
+					{
+						errors.Add(error.Description);
+					}
+			}
+
+            ViewBag.Errors = errors;
+
+            if(errors.Count == 0)
+            {
+				var result = _branchService.Add(branch);
+
+				if (result.Success)
+					return RedirectToAction("Index", "Home");
+                else
+					errors.Add(result.Message);
+			    ViewBag.Errors = errors;
+			}
+
+            return View(branch);
         }
     }
 }
